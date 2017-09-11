e6338e9502c53a26b63f9f49b87660542687784b
martincapo <martincapo@yahoo.com>
2017-09-11 17:55:55 +0000

    Step2

---
+++ getAndPrintHTML.js
@@ -0,0 +1,32 @@
+function getAndPrintHTML () {
+
+  var requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step2.html'
+  };
+
+  /* Add your code here */
+
+  var https = require('https');
+  var output = '';
+
+  https.get(requestOptions, function (response) {
+     // set encoding of received data to UTF-8
+    response.setEncoding('utf8');
+
+    // the callback is invoked when a `data` chunk is received
+    response.on('data', function (data) {
+      output += data;
+    });
+
+    // the callback is invoked when all of the data has been received
+    // (the `end` of the stream)
+    response.on('end', function() {
+      console.log(output);
+    });
+
+  });
+
+}
+
+getAndPrintHTML();
