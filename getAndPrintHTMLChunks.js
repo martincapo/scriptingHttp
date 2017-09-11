a32ba3444c4037e2eec38da235d7fa8f68315965
martincapo <martincapo@yahoo.com>
2017-09-11 17:36:28 +0000

    Step1

--- getAndPrintHTMLChunks.js
+++ getAndPrintHTMLChunks.js
@@ -15,7 +15,7 @@ function getAndPrintHTMLChunks () {
 
     // the callback is invoked when a `data` chunk is received
     response.on('data', function (data) {
-      console.log('Chunk Received. Length:', data.length);
+      console.log('Chunk Received.', data, '\n');
     });
 
     // the callback is invoked when all of the data has been received
