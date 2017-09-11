822488457e22fd73d3bc8d191289bd6ce2fb9934
martincapo <martincapo@yahoo.com>
2017-09-11 18:07:39 +0000

    Step4

---
+++ getHTML.js
@@ -0,0 +1,35 @@
+function getHTML (options, callback) {
+
+  /* Add your code here */
+  var https = require('https');
+  var output = '';
+
+  https.get(requestOptions, function (response) {
+     // set encoding of received data to UTF-8
+    response.setEncoding('utf8');
+
+    // the callback is invoked when a `data` chunk is received
+    response.on('data', function (data) {
+      output += data;
+    });
+
+    // the callback is invoked when all of the data has been received
+    // (the `end` of the stream)
+    response.on('end', function() {
+      console.log(callback(output));
+    });
+
+  });
+
+}
+
+function printHTML (html) {
+  console.log(html);
+}
+
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step4.html'
+};
+
+getHTML(requestOptions, printHTML);
