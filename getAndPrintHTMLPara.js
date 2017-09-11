8e35eff2047f3d608a87fd94dc9af2c891082f13
martincapo <martincapo@yahoo.com>
2017-09-11 18:02:35 +0000

    Step3

---
+++ getAndPrintHTMLPara.js
@@ -0,0 +1,31 @@
+function getAndPrintHTML (options) {
+
+  /* Add your code here */
+  var https = require('https');
+  var output = '';
+
+  https.get(requestOptions, function (response) {
+     // set encoding of received data to UTF-8
+    response.setEncoding('utf8');
+
+    // the callback is invoked when a `data` chunk is received
+    response.on('data', function (data) {
+      output += data;
+    });
+
+    // the callback is invoked when all of the data has been received
+    // (the `end` of the stream)
+    response.on('end', function() {
+      console.log(output);
+    });
+
+  });
+
+}
+
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step3.html'
+};
+
+getAndPrintHTML(requestOptions);
